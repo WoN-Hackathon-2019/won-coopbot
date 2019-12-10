0e9d280d64bc46530d010d6e10f7d5ba8378994c
Dominik Scheffknecht <e1426857@student.tuwien.ac.at>
2019-12-10 14:43:00 +0100

    introduced data model for sport places

---
+++ src/main/java/won/bot/skeleton/persistence/model/Location.java
@@ -0,0 +1,26 @@
+package won.bot.skeleton.persistence.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Location {
+
+    private double longitude;
+    private double latitude;
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+}
---
+++ src/main/java/won/bot/skeleton/persistence/model/SportPlace.java
@@ -0,0 +1,68 @@
+package won.bot.skeleton.persistence.model;
+
+import javax.persistence.Entity;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class SportPlace {
+
+    private String id;
+    private Location location;
+    private String address;
+    private boolean outdoor;
+    private List<String> category;
+    private String weblink;
+
+    public SportPlace() {
+        this.category = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public boolean isOutdoor() {
+        return outdoor;
+    }
+
+    public void setOutdoor(boolean outdoor) {
+        this.outdoor = outdoor;
+    }
+
+    public List<String> getCategory() {
+        return category;
+    }
+
+    public void setCategory(List<String> category) {
+        this.category = category;
+    }
+
+    public String getWeblink() {
+        return weblink;
+    }
+
+    public void setWeblink(String weblink) {
+        this.weblink = weblink;
+    }
+}
