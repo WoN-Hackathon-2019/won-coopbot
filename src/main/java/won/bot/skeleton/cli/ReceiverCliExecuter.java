676398151d08f45a3c596c3c19467e37c91ca2cb
schokobaer <andreas.peter.fend@gmail.com>
2019-12-16 20:29:13 +0100

    ConnectMessage for LocationContainingAtoms; LocationService

--- src/main/java/won/bot/skeleton/cli/ReceiverCliExecuter.java
+++ src/main/java/won/bot/skeleton/cli/ReceiverCliExecuter.java
@@ -5,22 +5,38 @@ import at.apf.easycli.annotation.DefaultValue;
 import at.apf.easycli.annotation.Meta;
 import won.bot.framework.eventbot.EventListenerContext;
 import won.bot.framework.eventbot.bus.EventBus;
+import won.bot.framework.eventbot.event.impl.command.connectionmessage.ConnectionMessageCommandEvent;
 import won.bot.framework.eventbot.event.impl.wonmessage.MessageFromOtherAtomEvent;
 import won.bot.skeleton.event.CreateGroupChatEvent;
+import won.bot.skeleton.service.AtomLocationService;
+import won.protocol.model.Coordinate;
 
 public class ReceiverCliExecuter {
 
     private EventListenerContext ctx;
     private EventBus bus;
+    private AtomLocationService als;
 
     public ReceiverCliExecuter(EventListenerContext ctx, EventBus bus) {
         this.ctx = ctx;
         this.bus = bus;
+        als = new AtomLocationService(ctx);
     }
 
     @Command("/new")
     public void createNewGroup(String name, @DefaultValue("100") int capacity, @Meta MessageFromOtherAtomEvent event) {
         bus.publish(new CreateGroupChatEvent(name, event.getTargetSocketURI(), capacity));
+    }
+
+
+    @Command("/myloc")
+    public void getLocationOfAtom(@Meta MessageFromOtherAtomEvent event) {
+        Coordinate location = als.getAtomLocation(event.getTargetAtomURI());
+        if (location != null) {
+            bus.publish(new ConnectionMessageCommandEvent(event.getCon(), "Lon: " + location.getLongitude() + " / Lan: " + location.getLatitude()));
+        } else {
+            bus.publish(new ConnectionMessageCommandEvent(event.getCon(), "You don't have a location defined"));
+        }
     }
 
 
