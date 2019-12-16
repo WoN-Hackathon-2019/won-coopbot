f840d533bca1d1338a91243fc465d686ebde188d
Dominik Scheffknecht <e1426857@student.tuwien.ac.at>
2019-12-16 15:45:05 +0100

    impl command to remove group members

--- src/main/java/won/bot/skeleton/cli/GroupCliExecuter.java
+++ src/main/java/won/bot/skeleton/cli/GroupCliExecuter.java
@@ -3,11 +3,21 @@ package won.bot.skeleton.cli;
 import at.apf.easycli.annotation.Command;
 import at.apf.easycli.annotation.Meta;
 import won.bot.framework.eventbot.EventListenerContext;
+import won.bot.framework.eventbot.action.BaseEventBotAction;
+import won.bot.framework.eventbot.action.EventBotActionUtils;
 import won.bot.framework.eventbot.bus.EventBus;
+import won.bot.framework.eventbot.event.Event;
+import won.bot.framework.eventbot.event.impl.command.close.CloseCommandEvent;
+import won.bot.framework.eventbot.event.impl.command.close.CloseCommandResultEvent;
+import won.bot.framework.eventbot.event.impl.command.close.CloseCommandSuccessEvent;
 import won.bot.framework.eventbot.event.impl.command.connectionmessage.ConnectionMessageCommandEvent;
 import won.bot.framework.eventbot.event.impl.wonmessage.MessageFromOtherAtomEvent;
+import won.bot.framework.eventbot.filter.impl.CommandResultFilter;
+import won.bot.framework.eventbot.listener.EventListener;
+import won.bot.framework.eventbot.listener.impl.ActionOnFirstEventListener;
 import won.bot.skeleton.context.SkeletonBotContextWrapper;
 import won.bot.skeleton.model.GroupMember;
+import won.protocol.model.Connection;
 import won.protocol.util.linkeddata.WonLinkedDataUtils;
 
 import java.net.URI;
@@ -42,7 +52,7 @@ public class GroupCliExecuter {
         bus.publish(new ConnectionMessageCommandEvent(event.getCon(), nameChangeNotification));
     }
 
-    @Command("/listMembers")
+    @Command("/list")
     public void listGroupMembers(@Meta MessageFromOtherAtomEvent event) {
         List<GroupMember> groupMembers = wrapper.getGroupMembers(event.getAtomURI());
 
@@ -58,6 +68,42 @@ public class GroupCliExecuter {
         }
         bus.publish(new ConnectionMessageCommandEvent(event.getCon(), builder.toString()));
     }
+
+    @Command("/remove")
+    public void removeMember(String name, @Meta MessageFromOtherAtomEvent event) {
+        URI adminConnectionUri = wrapper.getGroup(event.getAtomURI()).getAdminConnectionUri();
+        if (!adminConnectionUri.equals(event.getConnectionURI())) {
+            bus.publish(new ConnectionMessageCommandEvent(event.getCon(), "Only the admin can remove users"));
+            return;
+        }
+        URI toRemoveConnection = wrapper.getGroupMembers(event.getAtomURI()).stream()
+                .filter(groupMember -> groupMember.getName().equals(name))
+                .map(GroupMember::getConnectionUri)
+                .findFirst()
+                .orElseGet(null);
+
+        if (toRemoveConnection == null) {
+            bus.publish(new ConnectionMessageCommandEvent(event.getCon(), "User with name " + name + "not found."));
+            return;
+        }
+
+        wrapper.removeGroupMember(event.getAtomURI(), toRemoveConnection);
+        Connection toCloseConnection = WonLinkedDataUtils.getConnectionForConnectionURI(toRemoveConnection, ctx.getLinkedDataSource()).orElseGet(null);
+        CloseCommandEvent closeCommandEvent = new CloseCommandEvent(toCloseConnection);
+        bus.subscribe(CloseCommandSuccessEvent.class, new ActionOnFirstEventListener(ctx, new CommandResultFilter(closeCommandEvent), new BaseEventBotAction(ctx) {
+            @Override
+            protected void doRun(Event event1, EventListener eventListener) throws Exception {
+                CloseCommandResultEvent closeCommandResultEvent = (CloseCommandSuccessEvent) event1;
+                if (!closeCommandResultEvent.isSuccess()) {
+                    bus.publish(new ConnectionMessageCommandEvent(event.getCon(), "Could nor remove user"));
+                } else {
+                    sendBroadcastMessage("Admin removed  " + name, event.getAtomURI(), null);
+                }
+            }
+        }));
+        bus.publish(closeCommandEvent);
+
+     }
 
     private void sendBroadcastMessage(String msg, URI atomUri, URI senderConUri) {
         wrapper.getGroupMembers(atomUri).stream()
