f7a0fdeb449cb57f9b152c827f502016ac60a1aa
unreinCoding <reini.unger@kabelplus.at>
2019-12-17 12:35:05 +0100

    missing import

--- src/main/java/won/bot/skeleton/cli/ReceiverCliExecuter.java
+++ src/main/java/won/bot/skeleton/cli/ReceiverCliExecuter.java
@@ -24,6 +24,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import won.bot.skeleton.persistence.model.SportPlace;
 import won.bot.skeleton.service.AtomLocationService;
 import won.protocol.model.Coordinate;
 import won.protocol.util.DefaultAtomModelWrapper;
